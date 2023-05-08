7e7c784479e5b5dc99669e67b3ed5f6ecb8e206f
YeongGuk <sin9173@frankit.shop>
2023-05-08 15:55:55 +0900

    init

---
+++ src/main/java/module/ncloud/alim/AlimApplication.java
@@ -0,0 +1,13 @@
+package module.ncloud.alim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/module/ncloud/alim/AlimApplicationTests.java
@@ -0,0 +1,13 @@
+package module.ncloud.alim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
